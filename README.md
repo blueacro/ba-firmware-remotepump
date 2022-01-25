9e7c772955af7ab928798467207ce61aeff7cafc
Yann Ramin <github@theatr.us>
2022-01-24 22:22:03 -0800

    Steps, screen, etc

--- README.md
+++ README.md
@@ -7,3 +7,40 @@ Firmware for a stepper pump auto water change hub
 - Install the compilation target for your MCU. Eg run `rustup target add thumbv7em-none-eabihf`.
 - Install flash and debug tools: `cargo install flip-link`, `cargo install probe-run`.
 - Connect your device. Run `cargo run --release` to compile and flash.
+
+# MCU Pinout
+
+- PA0 10 TXEN
+- PA1 11 TRQ
+- PA2 12 TX
+- PA3 13 RX
+- PA4 14 DIR
+- PA5 15 STEP
+- PA6 16 EN0
+- PA7 17 EN1
+- PA8 29 nSLEEP
+- PA9 30 VBUS
+- PA10 31 SEN2
+- PA11 32 USBDM
+- PA12 33 USBDP
+- PA13 34 SWDIO
+- PA14 37 SWCLK
+
+- PB0 18 M1
+- PB1 19 PB1
+- PB3 39 SWO
+- PB4 40 nFAULT1
+- PB5 41 nFAULT2
+- PB6 42 BOOT0 SW5
+- PB7 43 SDA
+- PB8 44 SCL
+- PB9 46 SW4
+- PB10 21 SW3
+- PB12 25 SW2
+- PB13 26 SW1
+- PB14 27 LED2
+- PB15 28 LED1
+
+- PC13 2 ROUT1 (relay FET)
+- PC14 3 ROUT2
+
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::otg_fs::{UsbBus, USB};
-use stm32f4xx_hal::{pac, prelude::*};
+use stm32f4xx_hal::{i2c::I2c, pac, prelude::*};
+
+use embedded_graphics::{image::Image, image::ImageRaw};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use sh1106::{prelude::*, Builder};
+
 use usb_device::prelude::*;
 
 // These lines are part of our setup for debug printing.
@@ -17,6 +27,7 @@ static mut EP_MEMORY: [u32; 1024] = [0; 1024];
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
     let rcc = dp.RCC.constrain();
 
@@ -30,12 +41,60 @@ fn main() -> ! {
     let gpioa = dp.GPIOA.split();
     let gpiob = dp.GPIOB.split();
 
-    let mut led_red = gpiob.pb4.into_push_pull_output();
-    let mut led_blue = gpiob.pb5.into_push_pull_output();
-    let mut led_green = gpioa.pa15.into_push_pull_output();
-    led_green.set_high();
-    led_blue.set_low();
-    led_red.set_low();
+    let mut led1 = gpiob.pb14.into_push_pull_output();
+    let mut led2 = gpiob.pb15.into_push_pull_output();
+
+    led1.set_high();
+    led2.set_low();
+
+    // Stepper control
+    let mut dir_pin = gpioa.pa4.into_push_pull_output();
+    dir_pin.set_low();
+    let mut step_pin = gpioa.pa5.into_push_pull_output();
+    step_pin.set_low();
+    let mut en0_pin = gpioa.pa6.into_push_pull_output();
+    en0_pin.set_low();
+    let mut en1_pin = gpioa.pa7.into_push_pull_output();
+    en1_pin.set_low();
+    let mut nsleep_pin = gpioa.pa8.into_push_pull_output();
+    nsleep_pin.set_high();
+    let mut m1_pin = gpiob.pb0.into_push_pull_output();
+    let mut m0_pin = gpiob.pb1.into_push_pull_output();
+    m0_pin.set_high();
+    m1_pin.set_low();
+
+    let scl = gpiob
+        .pb8
+        .into_alternate()
+        .internal_pull_up(true)
+        .set_open_drain();
+    let sda = gpiob
+        .pb7
+        .into_alternate()
+        .internal_pull_up(true)
+        .set_open_drain();
+    let i2c = I2c::new(dp.I2C1, (scl, sda), 400.khz(), &clocks);
+
+    let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+    display.init().unwrap();
+    display.clear();
+    display.flush().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
 
     let usb = USB {
         usb_global: dp.OTG_FS_GLOBAL,
@@ -51,13 +110,22 @@ fn main() -> ! {
     let mut serial = usbd_serial::SerialPort::new(&usb_bus);
 
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
+        .manufacturer("blueAcro")
+        .product("RemotePump")
         .serial_number("TEST")
         .device_class(usbd_serial::USB_CLASS_CDC)
         .build();
 
+    let mut delay = stm32f4xx_hal::delay::Delay::new(cp.SYST, &clocks);
+    let mut delay_us = 200_u32;
+    en0_pin.set_high();
     loop {
+        step_pin.toggle();
+        delay.delay_us(delay_us);
+        if delay_us > 40_u32 {
+            delay_us -= 1;
+        }
+
         if !usb_dev.poll(&mut [&mut serial]) {
             continue;
         }
@@ -88,7 +156,6 @@ fn main() -> ! {
         }
     }
 }
-
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
